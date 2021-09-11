ef0d0ede74d049cc4f74e8ab31c3653e97082a9f
holequotin <69898497+holequotin@users.noreply.github.com>
2021-09-11 20:34:14 +0700

    Update operator overloading.cpp

--- operator overloading.cpp
+++ operator overloading.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
-
+#include <iostream>
+//format code :Shift + Alt +F
 using namespace std;
-struct  Point3D{
+struct Point3D
+{
     float x;
     float y;
     float z;
@@ -9,43 +10,56 @@ struct  Point3D{
 typedef Point3D point;
 //operator overloading
 //các argument toàn là các địa chỉ bởi vì hàm này là thay đổi giá trị của argument
-istream & operator >> (istream &din,point &p1){
-    cout<<"Nhap cac toa do x,y,z:";
-    din>>p1.x>>p1.y>>p1.z;
-    return(din);
+istream &operator>>(istream &din, point &p1)
+{
+    cout << "Nhap cac toa do x,y,z:";
+    din >> p1.x >> p1.y >> p1.z;
+    return (din);
 };
-ostream &operator << (ostream &dout,point p1){
-    dout<<p1.x<<" "<<p1.y<<" "<<p1.z<<endl;
+ostream &operator<<(ostream &dout, point p1)
+{
+    dout << p1.x << " " << p1.y << " " << p1.z << endl;
     return (dout);
 }
-point operator + (point p1, point p2){
+point operator+(point p1, point p2)
+{
     point p;
-    p.x=p1.x+p2.x;
-    p.y=p1.y+p2.y;
-    p.z=p1.z+p2.z;
+    p.x = p1.x + p2.x;
+    p.y = p1.y + p2.y;
+    p.z = p1.z + p2.z;
     return p;
 }
-point operator -(point p1,point p2){
+point operator-(point p1, point p2)
+{
     point p;
-    p.x=p1.x-p2.x;
-    p.y=p1.y-p2.y;
-    p.z=p1.z-p2.z;
+    p.x = p1.x - p2.x;
+    p.y = p1.y - p2.y;
+    p.z = p1.z - p2.z;
     return p;
 }
-bool operator ==(point p1,point p2){
-    if(p1.x!=p2.x||p1.y!=p2.y||p1.z!=p2.z) return false;
-    else return true;
+bool operator==(point p1, point p2)
+{
+    if (p1.x != p2.x || p1.y != p2.y || p1.z != p2.z)
+        return false;
+    else
+        return true;
 }
-bool operator !=(point p1,point p2){
-    if(p1.x!=p2.x||p1.y!=p2.y||p1.z!=p2.z) return true;
-    else return false;
+bool operator!=(point p1, point p2)
+{
+    if (p1.x != p2.x || p1.y != p2.y || p1.z != p2.z)
+        return true;
+    else
+        return false;
 }
-int main() {
-point p1,p2;
-cin>>p1>>p2;
-cout<<p1+p2;
-cout<<p1-p2;
-if(p1==p2) cout<<"2 diem bang nhau";
-else cout<<"2 diem khac nhau";
+int main()
+{
+    point p1, p2;
+    cin >> p1 >> p2;
+    cout << p1 + p2;
+    cout << p1 - p2;
+    if (p1 == p2)
+        cout << "2 diem bang nhau";
+    else
+        cout << "2 diem khac nhau";
     return 0;
 }
